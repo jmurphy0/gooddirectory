ae5bca84f96c6427a500bb4ad5eadc3ccfab34a9
jmurphy0 <murphbot55@gmail.com>
2021-03-24 15:29:03 -0400

    commiting dataTable

---
+++ src/components/dataTable/index.js
@@ -0,0 +1,71 @@
+import React from "react";
+// importing MDBD datatable for ease of sorting and searching
+import { MDBDataTable } from "mdbreact";
+
+const DataTable = (props) => {
+  // the const data will assign MDBDataTable an object of column and row data
+  const data = {
+    // defining what the columns will be
+    columns: [
+      {
+        label: "Name",
+        field: "name",
+        sort: "asc",
+        searchable: "true",
+      },
+      {
+        label: "Email",
+        field: "email",
+        sort: "asc",
+      },
+      {
+        label: "Age",
+        field: "age",
+        sort: "asc",
+      },
+      {
+        label: "Phone",
+        field: "phone",
+        sort: "disabled",
+      },
+      {
+        label: "Cell",
+        field: "cell",
+        sort: "asc",
+      },
+      {
+        label: "City",
+        field: "city",
+        sort: "asc",
+      },
+      {
+        label: "Country",
+        field: "country",
+        sort: "asc",
+      },
+      {
+        label: "Img",
+        field: "img",
+        sort: "disabled",
+      },
+    ],
+    rows: props.employees,
+  };
+  console.log("props", props.employees);
+  console.log("data", data);
+  return (
+    <MDBDataTable
+      searchtop="true"
+      //searchBottom="false"
+      responsive
+      striped
+      hover
+      sorting="true"
+      bordered
+      data={data}
+      info={true}
+    />
+  );
+};
+
+export default DataTable;
