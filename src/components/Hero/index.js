334531023a2cbba45910ae3eaee8524d7474c6eb
jmurphy0 <murphbot55@gmail.com>
2021-03-24 16:03:19 -0400

    adding hero + hero image

---
+++ src/components/Hero/index.js
@@ -0,0 +1,17 @@
+import "./style.css";
+import Background from "../assets/images/DigitalImage.jpg";
+import React from "react";
+
+function Hero() {
+  return (
+    <div className="hero text-center">
+      <Background />
+      <h1 id="title">Employee Directory</h1>
+      <p className="text">
+        Search the directory or click the column you'd like to sort
+      </p>
+    </div>
+  );
+}
+
+export default Hero;
